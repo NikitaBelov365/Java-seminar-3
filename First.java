cd8a7732841579be3d2dee7d250fb0420b227946
Nikitabelov365 <nikitabelov365@gmail.com>
2023-07-02 00:15:15 +0300

    task 1 solved

---
+++ First.java
@@ -0,0 +1,16 @@
+public class First {
+
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = s1;
+        String s4 = "h" + "e" + "l" + "l" + "o";
+        String s5 = new String("hello");
+        String s6 = new String(new char[] { 'h', 'e', 'l', 'l', 'o' });
+
+
+        if (s1.equals(s2)) {
+            System.out.println("da");
+        }
+    }
+}
